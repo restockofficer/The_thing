cf537c2fde4c20067ce8c0ef21ac8b5c09bcba57
restockofficer <160753481+restockofficer@users.noreply.github.com>
2024-04-29 13:39:55 +0000

    Alterações de muitas coisas

--- Nave_move.js
+++ Nave_move.js
@@ -1,10 +1,60 @@
-const adiv = document.getElementById('mydiv');
-let leftpos = 0;
-
-function movediv(timestamp){
-  leftpos += 5;
-  adiv.style.left = leftpos + 'px';
-  requestAnimationFrame(movediv);
+var cor = 'blue';
+var cont = 1;
+var myGamePiece;
+function startGame() {
+myGamePiece = new component(30, 30, "red", 450, 0);
+myGameArea.start();
 }
 
-requestAnimationFrame(movediv);
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+  this.canvas.width = 480;
+  this.canvas.height = 270;
+  this.context = this.canvas.getContext("2d");
+  document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  }
+  
+  function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.color;
+this.color;
+this.update = function(){
+ctx = myGameArea.context;
+if(cont % 10 == 0)
+cor = 'red';
+cor = 'blue';
+
+//ctx.fillStyle = color;
+ctx.fillStyle = cor;
+ctx.fillRect(this.x, this.y, this.width, this.height);
+cont++;
+
+}
+}
+
+function updateGameArea(timestamp) {
+myGameArea.clear();
+myGamePiece.y += 1; 
+myGamePiece.x -= 2; 
+myGamePiece.update();
+if (myGamePiece) {x + Width >= canvas.width || y + Height >= canvas.height }{
+    cancelAnimationFrame(animationId); 
+    return;
+}
+
+animationId = requestAnimationFrame(updateGameArea);
+
+let animationId = requestAnimationFrame(updateGameArea);
+
+
+requestAnimationFrame(updateGameArea);
+}
